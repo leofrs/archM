dc6346f698fee73d539bb54f27f6c3a672564b4a
Leonardo Rodrigues <leofrrodrigues86@gmail.com>
2026-07-24 15:15:55 -0300

    Fix: adiciona propriedade base no vite config para o gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/archM/", //adicione esta linha exatamente assim
 });
